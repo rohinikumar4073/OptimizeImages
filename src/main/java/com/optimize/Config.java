504c3382c8047d469d171231b290149ad7b40783
vishnu sandhireddy <vissu_nbkr@yahoo.com>
2018-02-25 13:08:27 +0530

    Add MYSQL config

--- src/main/java/com/optimize/Config.java
+++ src/main/java/com/optimize/Config.java
@@ -1,8 +1,22 @@
 package com.optimize;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Config {
-public static String databaseUrl="jdbc:mysql://localhost/optimize";
-public static String userName="root";
-public static String passWord="Ch@ngeIsG00d";
-public static String fileLocation="/Users/VISHNU/Images";
+	public static File temp ;
+static {
+	 try {
+		temp = File.createTempFile("temp-file-name", ".tmp");
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+
 }
+public static String databaseUrl="jdbc:mysql://us-cdbr-iron-east-05.cleardb.net/heroku_b71e4549731517b";
+public static String userName="be22c0d42b69d9";
+public static String passWord="1719164f";
+public static String fileLocation=temp.getAbsolutePath();
+}
+
